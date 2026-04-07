d6cd319e20f8dbda87cad54f664ec2a29b3a3e52
o-ga09 <taitiabe1997@gmail.com>
2026-04-07 21:59:08 +0900

    chore: ファイルツリーの設定を修正

---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,21 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    explorer = {
+      hidden = true,
+    },
+    picker = {
+      sources = {
+        grep = {
+          hidden = true,
+        },
+        explorer = {
+          hidden = true,
+        },
+        files = {
+          hidden = true,
+        },
+      },
+    },
+  },
+}
